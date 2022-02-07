b3ec95c3b3c852cd783c2c9ef6aeadb7294f42d0
Walter Shub <waltershub@gmail.com>
2022-02-07 16:31:55 -0500

    added yes no button

--- src/components/topicFlatlist.js
+++ src/components/topicFlatlist.js
@@ -30,38 +30,62 @@ const TopicFlatlist = ({apiUrl}) => {
 };
 
 const TopicRenderItem = ({item}) => (
-  <View
-    style={{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginHorizontal: scale(25),
-    }}>
+  <View style={styles.topicRenderItemContainer}>
     <Avatar user={item.user[0]} />
-    <View
-      style={{
-        borderRadius: 5,
-        backgroundColor: '#FFF',
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        elevation: 3,
-        borderColor: '#000',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: scale(250),
-        height: verticalScale(100),
-        marginVertical: verticalScale(10),
-      }}>
+    <View style={styles.topicCardContainer}>
       <Text>{item.argument}</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '50%',
+        }}>
+        <YesNoButton stance="yes" voteCount={item.upvoteCount}></YesNoButton>
+        <YesNoButton stance="no" voteCount={item.downvoteCount}></YesNoButton>
+      </View>
     </View>
   </View>
 );
 
+const YesNoButton = ({stance, voteCount}) => {
+  const types = {
+    yes: {
+      title: 'YES',
+      background: '#e8f1ff',
+      color: '#1774ff',
+    },
+    no: {
+      title: 'NO',
+      background: '#ffe8e8',
+      color: '#ff0404',
+    },
+  };
+
+  const titleAndCount = `${types[stance].title} (${voteCount})`;
+
+  return (
+    <View
+      style={[
+        {
+          height: verticalScale(20),
+          width: scale(50),
+          borderRadius: scale(10),
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: verticalScale(20),
+        },
+        {backgroundColor: types[stance].background},
+      ]}>
+      <Text
+        style={[
+          {fontSize: moderateScale(10), fontWeight: 'bold'},
+          {color: types[stance].color},
+        ]}>
+        {titleAndCount}
+      </Text>
+    </View>
+  );
+};
 const Avatar = ({user}) => {
   return (
     <View
@@ -94,7 +118,7 @@ const Avatar = ({user}) => {
           }}
         />
       ) : (
-        <Text style={{color: '#fff'}}>{user.name[0]}</Text>
+        <Text style={{color: '#fff'}}>{user.name[0].toUpperCase()}</Text>
       )}
     </View>
   );
@@ -117,6 +141,32 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   listEmptyTextStyle: {fontSize: moderateScale(15), fontWeight: 'bold'},
+  topicRenderItemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: scale(25),
+  },
+  topicCardContainer: {
+    borderRadius: scale(5),
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    justifyContent: 'space-between',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    elevation: 3,
+    borderColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: scale(250),
+    height: verticalScale(150),
+    marginVertical: verticalScale(10),
+    paddingHorizontal: scale(10),
+  },
 });
 let sampleTopics = [
   {
